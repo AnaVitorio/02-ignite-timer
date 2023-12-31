4b0f02ea069c7b6348cfe881b1c879f0a9d1ac29
Ana Carolina <anavitorio.p@gmail.com>
2023-12-31 20:21:15 -0300

    criando rotas iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Button } from "./componentes/Button";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="secondary"/>
-      <Button variant="primary"/>
-      <GlobalStyle/>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
-
-
---
+++ src/pages/History.tsx
@@ -0,0 +1,5 @@
+export function History(){
+    return(
+        <h1>Page History</h1>
+    )
+}
